c00411b933579f609c503560bc15ae1a7bdfde4f
Eli McGowan <eli.mcgowan@gmail.com>
2015-12-05 18:31:57 -0500

    Implemented method to find Aliquot cycles.

--- main/java/src/math/gaussianinteger/Main.java
+++ main/java/src/math/gaussianinteger/Main.java
@@ -1,8 +1,12 @@
 package math.gaussianinteger;
 
+
 public class Main {
 
+	private static GaussianInteger one = new GaussianInteger(1, 0);
+	
 	private GaussianIntegerFactorer gaussianIntegerFactorer;
+	private GaussianIntegerComparator gaussianIntegerComparator;
 	
 	public static void main(String[] args) {
 		Main main = new Main(new GaussianIntegerFactorer_BruteForce());
@@ -11,6 +15,7 @@ public class Main {
 	
 	public Main(GaussianIntegerFactorer gaussianIntegerFactorer) {
 		this.gaussianIntegerFactorer = gaussianIntegerFactorer;
+		this.gaussianIntegerComparator = new GaussianIntegerComparator();
 	}
 	
 	public void testGaussians(int startNum, int endNum){
@@ -25,26 +30,57 @@ public class Main {
 	}
 
 	public void testGaussian(long a, long b){
-		testGaussian(new GaussianInteger(a, b));
+		findAliquotCycle(new GaussianInteger(a, b));
 	}
 	
-	public void testGaussian(GaussianInteger gaussianInteger){
+	public void findPerfect(GaussianInteger gaussianInteger){
 		GaussianInteger divisorsSum = gaussianIntegerFactorer.getPositiveDivisorsSum(gaussianInteger);
-		
 		// Perfect number test
 		if(gaussianInteger.equals(divisorsSum)){
 			System.out.println("Perfect!: " + gaussianInteger);
 			System.exit(0);
 		}
+	}
+	
+	public void findAmicable(GaussianInteger gaussianInteger){
+		GaussianInteger divisorsSum = gaussianIntegerFactorer.getPositiveDivisorsSum(gaussianInteger);
 		
-		// Amicable number test
 		GaussianInteger nextSum = gaussianIntegerFactorer.getPositiveDivisorsSum(divisorsSum);
 		if(gaussianInteger.equals(nextSum)){
 			System.out.println("Amicable!: " + gaussianInteger + ", " + divisorsSum);
 			System.exit(0);
 		}
+	}
+	
+	/**
+	 * Also finds perfect numbers and amicable numbers.
+	 */
+	public void findAliquotCycle(GaussianInteger gaussianInteger){
+		findAliquotCycle(gaussianInteger, gaussianInteger);
+	}
+	
+	/**
+	 * Also finds perfect numbers and amicable numbers.
+	 */
+	public void findAliquotCycle(GaussianInteger startNumber, GaussianInteger currentNumber){
+		System.out.println("findAliquotSequence: " + startNumber + " - " + currentNumber);
 		
-		// Aliquot Sequence test
-		// TODO: Aliquot Sequence test
+		GaussianInteger sum = gaussianIntegerFactorer.getPositiveDivisorsSum(currentNumber);
+		
+		if(gaussianIntegerComparator.compare(sum, startNumber) > 0){
+			// stop searching when sum exceeds start number - this prevents trailing off into infinity
+			System.out.println("stop searching - sum > startNumber: " + sum + " > " + startNumber);
+			return;
+		}
+		if(sum.equals(startNumber)){
+			// This doesn't keep track of the sequence, so this will simply be a starting point
+			System.out.println("Aliquot found! " + startNumber);
+			System.exit(0);
+		}
+		if(sum.equals(one)){
+			System.out.println("stop searching - prime in sequence: " + startNumber);
+			return;
+		}
+		findAliquotCycle(startNumber, sum);
 	}
 }
